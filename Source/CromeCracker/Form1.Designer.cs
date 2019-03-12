964ded37b61fafcff3a66469e28457ccc82bf859
Bouletmarc <bouletmarc@hotmail.com>
2019-03-12 19:08:14 -0400

    Add files via upload

---
+++ Source/CromeCracker/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CromeCracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Logs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(40, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Crome.exe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_Logs
+            // 
+            this.textBox_Logs.Location = new System.Drawing.Point(7, 98);
+            this.textBox_Logs.Multiline = true;
+            this.textBox_Logs.Name = "textBox_Logs";
+            this.textBox_Logs.ReadOnly = true;
+            this.textBox_Logs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Logs.Size = new System.Drawing.Size(259, 110);
+            this.textBox_Logs.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Made by BM Devs (Bouletmarc)";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "exe";
+            this.saveFileDialog1.FileName = "crome_dealer.exe";
+            this.saveFileDialog1.Filter = "Crome Tuning Dealer|*.exe";
+            this.saveFileDialog1.Title = "Save Crome Cracked";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "exe";
+            this.openFileDialog1.FileName = "crome.exe";
+            this.openFileDialog1.Filter = "Crome Tuning|*.exe";
+            this.openFileDialog1.Title = "Locate Crome Tuning";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 227);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(272, 10);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(40, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Crack";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(143, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Uncrack";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(272, 237);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Logs);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Crome Tuning Cracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_Logs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Source/CromeCracker/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CromeCracker
+{
+    public partial class Form1 : Form
+    {
+        public string Version = "V1.0.0";
+        public byte[] Buffer;
+
+        public List<string> LocationIndex;
+        public List<int> LocationMode;
+
+        public List<string> LocationIndexCracked;
+        public List<int> LocationModeCracked;
+
+        public byte[] Find1 = new byte[7] { 0x72, 0x0F, 0x74, 0x2F, 0xFE, 0xC8, 0x74 };     // -5
+        public byte[] Find2 = new byte[7] { 0x74, 0x0C, 0xEB, 0x5D, 0xC6, 0x80, 0x65 };     // -2
+        public byte[] Find3 = new byte[7] { 0x75, 0x54, 0xA1, 0xA0, 0x17, 0x5A, 0x00 };     // -1
+        public byte[] Find4 = new byte[7] { 0x75, 0x14, 0x8D, 0x55, 0xF4, 0x8B, 0x83 };     // -1
+        public byte[] Find5 = new byte[7] { 0x0F, 0x84, 0x98, 0x00, 0x00, 0x00, 0x8D };     // -1
+
+        public byte[] Crack1 = new byte[7] { 0x90, 0x90, 0x90, 0x90, 0xFE, 0xC8, 0xEB };    // +5
+        public byte[] Crack2 = new byte[7] { 0x90, 0x90, 0xEB, 0x5D, 0xC6, 0x80, 0x65 };    // +2
+        public byte[] Crack3 = new byte[7] { 0xEB, 0x54, 0xA1, 0xA0, 0x17, 0x5A, 0x00 };    // +1
+        public byte[] Crack4 = new byte[7] { 0x74, 0x14, 0x8D, 0x55, 0xF4, 0x8B, 0x83 };    // +1
+        //public byte[] Crack4 = new byte[7] { 0xEB, 0x14, 0x8D, 0x55, 0xF4, 0x8B, 0x83 };    // +1
+        public byte[] Crack5 = new byte[7] { 0x0F, 0x85, 0x98, 0x00, 0x00, 0x00, 0x8D };     // +1
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            Log_This("Crome Cracker Interface Loaded " + Version);
+            Log_This("Made by BM Devs (Bouletmarc)");
+        }
+
+        public void Log_This(string Message)
+        {
+            textBox_Logs.AppendText(Message);
+            textBox_Logs.AppendText(Environment.NewLine);
+        }
+
+        private string GetHexLocation(int ThisNumber)
+        {
+            string XNumber = "2";
+
+            if (ThisNumber > 255)
+                XNumber = "2";
+            if (ThisNumber > 4095)
+                XNumber = "3";
+            if (ThisNumber > 65535)
+                XNumber = "4";
+            if (ThisNumber > 1048575)
+                XNumber = "5";
+            if (ThisNumber > 16777215)
+                XNumber = "6";
+            if (ThisNumber > 268435455)
+                XNumber = "7";
+
+            return ThisNumber.ToString("X" + XNumber);
+        }
+
+        private void Crack()
+        {
+            Log_This("Cracking...");
+
+            for (int index = 0; index < LocationIndex.Count; index++)
+            {
+                int Location = int.Parse(LocationIndex[index]);
+                int Mode = LocationMode[index];
+
+                byte[] LookupArray = new byte[0];
+
+                if (Mode == 0) LookupArray = Crack1;
+                if (Mode == 1) LookupArray = Crack2;
+                if (Mode == 2) LookupArray = Crack3;
+                if (Mode == 3) LookupArray = Crack4;
+                if (Mode == 4) LookupArray = Crack5;
+
+                for (int i = 0; i < LookupArray.Length; i++)
+                    Buffer[Location + i] = LookupArray[i];
+
+                if (LocationIndex.Count > 1)
+                    progressBar1.Value = (index * 100) / (LocationIndex.Count - 1);
+                else
+                    progressBar1.Value = 50;
+            }
+
+            Log_This("Cracking DONE");
+
+            progressBar1.Value = 0;
+
+            Save();
+        }
+
+        private void UnCrack()
+        {
+            Log_This("Uncracking...");
+
+            for (int index = 0; index < LocationIndexCracked.Count; index++)
+            {
+                int Location = int.Parse(LocationIndexCracked[index]);
+                int Mode = LocationModeCracked[index];
+
+                byte[] LookupArray = new byte[0];
+
+                if (Mode == 0) LookupArray = Find1;
+                if (Mode == 1) LookupArray = Find2;
+                if (Mode == 2) LookupArray = Find3;
+                if (Mode == 3) LookupArray = Find4;
+                if (Mode == 4) LookupArray = Find5;
+
+                for (int i = 0; i < LookupArray.Length; i++)
+                    Buffer[Location + i] = LookupArray[i];
+
+                if (LocationIndexCracked.Count > 1)
+                    progressBar1.Value = (index * 100) / (LocationIndexCracked.Count - 1);
+                else
+                    progressBar1.Value = 50;
+            }
+
+            Log_This("Cracking DONE");
+
+            progressBar1.Value = 0;
+
+            Save();
+        }
+
+        private void CheckCompatible()
+        {
+            LocationIndex = new List<string>();
+            LocationMode = new List<int>();
+
+            LocationIndexCracked = new List<string>();
+            LocationModeCracked = new List<int>();
+
+            bool CanBeCracked = false;
+            bool AlreadyCracked = false;
+
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            Log_This("Checking Compatibility");
+
+            progressBar1.Value = 0;
+
+            for (int Index = 0; Index < 5; Index++)
+            {
+                byte[] LookupArray = new byte[0];
+                byte[] LookupArrayCracked = new byte[0];
+
+                if (Index == 0)
+                {
+                    LookupArray = Find1;
+                    LookupArrayCracked = Crack1;
+                }
+                if (Index == 1)
+                {
+                    LookupArray = Find2;
+                    LookupArrayCracked = Crack2;
+                }
+                if (Index == 2)
+                {
+                    LookupArray = Find3;
+                    LookupArrayCracked = Crack3;
+                }
+                if (Index == 3)
+                {
+                    LookupArray = Find4;
+                    LookupArrayCracked = Crack4;
+                }
+                if (Index == 4)
+                {
+                    LookupArray = Find5;
+                    LookupArrayCracked = Crack5;
+                }
+
+                for (int i = 0; i < Buffer.Length; i++)
+                {
+
+                    progressBar1.Value = (i * 100) / (Buffer.Length - 1);
+
+                    if (Buffer[i] == LookupArray[0])
+                    {
+                        bool IsMatchingLookup = true;
+
+                        for (int i2 = 0; i2 < LookupArray.Length; i2++)
+                            if (Buffer[i + i2] != LookupArray[i2])
+                                IsMatchingLookup = false;
+
+                        if (IsMatchingLookup)
+                        {
+                            LocationIndex.Add(i.ToString());
+                            LocationMode.Add(Index);
+                            CanBeCracked = true;
+                        }
+                    }
+
+                    if (Buffer[i] == LookupArrayCracked[0])
+                    {
+                        bool IsMatchingLookup = true;
+
+                        for (int i2 = 0; i2 < LookupArrayCracked.Length; i2++)
+                            if (Buffer[i + i2] != LookupArrayCracked[i2])
+                                IsMatchingLookup = false;
+
+                        if (IsMatchingLookup)
+                        {
+                            LocationIndexCracked.Add(i.ToString());
+                            LocationModeCracked.Add(Index);
+                            AlreadyCracked = true;
+                        }
+                    }
+                }
+            }
+
+            progressBar1.Value = 0;
+
+            if (CanBeCracked)
+            {
+                button2.Enabled = true;
+
+                if (AlreadyCracked)
+                {
+                    Log_This("This file is partially cracked");
+                    button3.Enabled = true;
+                }
+                else
+                    Log_This("This file can be cracked");
+            }
+            else
+            {
+                if (AlreadyCracked)
+                {
+                    Log_This("This file is already cracked");
+                    button3.Enabled = true;
+                }
+                else
+                    Log_This("This file CAN'T be cracked");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Buffer = File.ReadAllBytes(openFileDialog1.FileName);
+                Log_This("File : " + Path.GetFileName(openFileDialog1.FileName) + " loaded");
+                Log_This("File Size (integer/hex) : " + Buffer.Length + "/0x" + GetHexLocation(Buffer.Length));
+                CheckCompatible();
+            }
+        }
+
+        private void Save()
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    File.Create(saveFileDialog1.FileName).Dispose();
+                    File.WriteAllBytes(saveFileDialog1.FileName, Buffer);
+
+                    Log_This("File : " + Path.GetFileName(saveFileDialog1.FileName) + " saved");
+                }
+                catch
+                {
+                    Log_This("ERROR : CAN'T write file at the specified location");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Crack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UnCrack();
+        }
+    }
+}
